c965e197db2efb9e2ff9072bde5377d3de2ba668
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-03 10:56:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int stockBuySell(vector<int> &arr) {
+        int n=arr.size();
+        int max_profit=0;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>arr[i-1])
+            {
+                max_profit=max_profit+(arr[i]-arr[i-1]);
+            }
+        }
+        return max_profit;
+        
+    }
+};
